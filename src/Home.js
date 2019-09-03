c74520f355ac3c3d9f1d80e2bf5faebff9fa684c
Krassimir Dimitrov <Krassimir.Dimitrov@akqa.com>
2019-09-03 16:18:11 +0100

    Added Contentful client

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,35 @@
 // Home.js
 import React from 'react';
+import { createClient } from 'contentful';
 
 class Home extends React.Component {
+	state = {
+		posts : []
+	};
+
+	componentWillMount() {
+		const client = createClient({
+			space       : process.env.REACT_APP_SPACE_ID,
+			accessToken : process.env.REACT_APP_ACCESS_TOKEN
+		});
+
+		client
+			.getEntries({})
+			.then((response) => {
+				this.setState({
+					posts : response.items
+				});
+			})
+			.catch(console.error);
+	}
+
 	render() {
-		return <p>Home</p>;
+		if (!this.state.posts.length) return <p>No posts found.</p>;
+
+		return this.state.posts.map((post) => {
+			console.log(post);
+			return <p>Post</p>;
+		});
 	}
 }
 
